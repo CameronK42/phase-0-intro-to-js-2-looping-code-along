79f64b7f4d8c8be642e82e4b18863c810add3c4a
Cameron <cameron.kawato@gmail.com>
2024-02-11 09:55:25 -0700

    passes the looping lap

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    messages.push(message);
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    debugger;
+  } 
+  return messages;
+}
+
+function countDown() {
+   let countDown = 10
+    while (countDown >= 0){
+        console.log(countDown);
+        countDown--;
+    }
+}
